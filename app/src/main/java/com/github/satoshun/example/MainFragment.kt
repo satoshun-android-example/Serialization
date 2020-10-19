2af6f9a76af020405fd3493522c029c797592f95
SatoShun <shun.sato1@gmail.com>
2020-10-19 05:40:39 +0000

    contextual

--- app/src/main/java/com/github/satoshun/example/MainFragment.kt
+++ app/src/main/java/com/github/satoshun/example/MainFragment.kt
@@ -4,6 +4,14 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.github.satoshun.example.databinding.MainFragBinding
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.Serializer
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainFragment : Fragment(R.layout.main_frag) {
   private val binding: MainFragBinding get() = MainFragBinding.bind(requireView())
---
+++ app/src/test/java/com/github/satoshun/example/DateTest.kt
@@ -0,0 +1,59 @@
+//@file:UseSerializers(DateSerializer::class)
+
+package com.github.satoshun.example
+
+import kotlinx.serialization.*
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.modules.SerializersModule
+import kotlinx.serialization.modules.plus
+import org.junit.Test
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
+
+class DateTest {
+  val time = "2020-09-08T09:34:14.836Z"
+
+  val date = """{"date": "$time"}"""
+
+  @Test
+  fun test() {
+    println(Json.decodeFromString<DateResponse>(date))
+  }
+
+  @Test
+  fun test2() {
+    val json = Json {
+      serializersModule += SerializersModule {
+        contextual(Date::class, DateSerializer)
+      }
+    }
+    println(json.decodeFromString<DateResponse2>(date))
+  }
+}
+
+@Serializer(forClass = Date::class)
+object DateSerializer : KSerializer<Date> {
+  private val df: DateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+
+  override fun deserialize(decoder: Decoder): Date {
+    val date = decoder.decodeString()
+    return df.parse(date)
+  }
+
+  override fun serialize(encoder: Encoder, value: Date) {
+    encoder.encodeString(value.time.toString())
+  }
+}
+
+@Serializable
+data class DateResponse(
+  @Serializable(with = DateSerializer::class) val date: Date
+)
+
+@Serializable
+data class DateResponse2(
+  @Contextual val date: Date
+)
