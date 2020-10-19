662cee2f634362c2a780b41d35670b56c2f90c2e
SatoShun <shun.sato1@gmail.com>
2020-10-19 04:36:31 +0000

    test

--- app/src/main/java/com/github/satoshun/example/AppActivity.kt
+++ app/src/main/java/com/github/satoshun/example/AppActivity.kt
@@ -1,5 +1,24 @@
 package com.github.satoshun.example
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
 
-class AppActivity : AppCompatActivity(R.layout.app_act)
+class AppActivity : AppCompatActivity(R.layout.app_act) {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    val hoge = """{"name": "hoge"}"""
+    val fuga = """{"name": "fuga"}"""
+
+    val hogeJson = Json.decodeFromString<Response1>(hoge)
+    println(hogeJson)
+  }
+}
+
+@Serializable
+class Response1(
+  val name: String
+)
---
+++ app/src/test/java/com/github/satoshun/example/SerializationTest.kt
@@ -0,0 +1,79 @@
+package com.github.satoshun.example
+
+import com.google.gson.Gson
+import com.google.gson.annotations.SerializedName
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import org.junit.Test
+
+class SerializationTest {
+  val hoge = """{"name": "hoge"}"""
+  val fuga = """{"name": "fuga"}"""
+  val unknown = """{"name": "unknown"}"""
+
+  @Test
+  fun test() {
+    println(Json.decodeFromString<Response1>(hoge))
+    println(Json.decodeFromString<Response1>(fuga))
+    println(Gson().fromJson(hoge, Response1::class.java))
+    println(Gson().fromJson(fuga, Response1::class.java))
+  }
+
+  @Test
+  fun test2() {
+    val json = Json {
+      ignoreUnknownKeys = true
+      coerceInputValues = true
+    }
+    println(json.decodeFromString<Response2>(hoge))
+    println(json.decodeFromString<Response2>(fuga))
+    println(json.decodeFromString<Response2>(unknown))
+  }
+
+  @Test
+  fun test3() {
+    val json = Json {
+      ignoreUnknownKeys = true
+      coerceInputValues = true
+    }
+    println(json.decodeFromString<Response2>(hoge))
+    println(json.decodeFromString<Response2>(fuga))
+    println(json.decodeFromString<Response2>(unknown))
+  }
+
+  @Test
+  fun test4() {
+    println(Gson().fromJson(hoge, Response3::class.java))
+    println(Gson().fromJson(fuga, Response3::class.java))
+    println(Gson().fromJson(unknown, Response3::class.java))
+  }
+}
+
+@Serializable
+data class Response1(
+  val name: String
+)
+
+@Serializable
+data class Response2(
+  val name: Name = Name.EMPTY
+)
+
+@Serializable
+enum class Name {
+  @SerialName("hoge") HOGEHGOE,
+  @SerialName("fuga") FUGAFUGA,
+  EMPTY
+}
+
+data class Response3(
+  val name: Name2?
+)
+
+enum class Name2 {
+  @SerializedName("hoge") HOGEHGOE,
+  @SerializedName("fuga") FUGAFUGA,
+  EMPTY
+}
