659dba8ce224f59f93530b94c8fe84fe44cdef22
SatoShun <shun.sato1@gmail.com>
2020-10-22 01:58:41 +0000

    inline clas

---
+++ app/src/test/java/com/github/satoshun/example/InlineTest.kt
@@ -0,0 +1,20 @@
+package com.github.satoshun.example
+
+import kotlinx.serialization.Serializable
+import org.junit.Test
+
+class InlineTest {
+  @Test
+  fun test() {
+    TODO("not supported yet")
+  }
+
+//  @Serializable
+//  data class Response(
+//    val a: InlineResponse
+//  )
+}
+
+//inline class InlineResponse(
+//  val value: String
+//)
